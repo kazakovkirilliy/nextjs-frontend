6fd07bae320b3dfe8518a8a0aa722831b7367bfc
Kirill Kazakov <52775272+kazakovkirilliy@users.noreply.github.com>
2023-01-10 22:22:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,14 +8,12 @@ module.exports = (phase) => {
         domains: ['images.unsplash.com', 'dummyimage.com', 'res.cloudinary.com', 'api.mapbox.com'],
       },
       env: {
-        MAPBOX_USER: 'kazakovkirilliy',
-        MAPBOX_STYLE:'cl1ezg2vy003l15qsx7o78fjp',
-        CLOUDINARY_CLOUD_NAME:'djwbrzqjm',
-        MAPS: 'pk.eyJ1Ijoia2F6YWtvdmtpcmlsbGl5IiwiYSI6ImNsMWV6aXc2YTBueG8zaWxubmwxb3V0N3QifQ.bjZkUjBK0K1qfY-63VpWIg',
-        // BACKEND_URL_HTTP:'http://localhost:4000/graphql',
-        // BACKEND_URL_WS:'ws://localhost:4000/graphql',
-        BACKEND_URL_HTTP:'https://potkame.herokuapp.com/graphql',
-        BACKEND_URL_WS:'wss://potkame.herokuapp.com/graphql',
+        MAPBOX_USER: '',
+        MAPBOX_STYLE:'',
+        CLOUDINARY_CLOUD_NAME:'',
+        MAPS: '',
+        BACKEND_URL_HTTP:'',
+        BACKEND_URL_WS:'',
       },
     }
   }
@@ -25,12 +23,12 @@ module.exports = (phase) => {
       domains: ['res.cloudinary.com', 'api.mapbox.com'],
     },
     env: {
-      MAPBOX_USER: 'kazakovkirilliy',
-      MAPBOX_STYLE:'cl1ezg2vy003l15qsx7o78fjp',
-      CLOUDINARY_CLOUD_NAME:'djwbrzqjm',
-      MAPS: 'pk.eyJ1Ijoia2F6YWtvdmtpcmlsbGl5IiwiYSI6ImNsMWV6aXc2YTBueG8zaWxubmwxb3V0N3QifQ.bjZkUjBK0K1qfY-63VpWIg',
-      BACKEND_URL_HTTP:'https://potkame.herokuapp.com/graphql',
-      BACKEND_URL_WS:'wss://potkame.herokuapp.com/graphql',
+      MAPBOX_USER: '',
+      MAPBOX_STYLE:'',
+      CLOUDINARY_CLOUD_NAME:'',
+      MAPS: '',
+      BACKEND_URL_HTTP:'',
+      BACKEND_URL_WS:'',
     },
   }
 }
